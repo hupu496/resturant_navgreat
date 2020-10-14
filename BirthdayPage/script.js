71e60a0f557fcda329d1e9b728bd1ec67a554f85
Aaadarsh <aadarsh629@gmail.com>
2020-10-14 15:48:12 +0530

    quick work in animations
    hey its awesome

---
+++ BirthdayPage/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "swetu" && b=="19-11-2020")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
